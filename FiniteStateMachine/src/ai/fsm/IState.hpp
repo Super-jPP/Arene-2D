395c9c8586f2cda75c455a755449f23b032182e5
unknown <cjarjoura@gamingcampus.local>
2026-02-04 12:03:53 +0100

    ai: add generic IState interface

---
+++ FiniteStateMachine/src/ai/fsm/IState.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace ai::fsm {
+	template <typename Owner> 
+	class IState {
+	public: 
+		virtual ~IState() = default;
+
+		virtual void onEnter(Owner&) = 0;
+		virtual void onUpdate(Owner&, float dt) = 0;
+		virtual void onExit(Owner&) = 0;
+
+		virtual const char* name()const = 0;
+	};
+}
