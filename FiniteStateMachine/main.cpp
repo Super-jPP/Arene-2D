cee751d79f916cf0a84927d5cd06ac63c82efa9f
unknown <cjarjoura@gamingcampus.local>
2026-02-04 10:18:43 +0100

    refactor project

--- FiniteStateMachine/main.cpp
+++ FiniteStateMachine/main.cpp
@@ -1,16 +1,7 @@
-#include "StateMachine/NPC.h"
 #include <SFML/Graphics.hpp>
+
 int main()
 {
-    //Npc npc;
-
-    //npc.Init();
-
-    //while (true)
-    //{
-    //    npc.Update();
-    //}
-
     sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
@@ -28,11 +19,3 @@ int main()
         window.display();
     }
 }
-
-
-/*
-Template
-FSM
-fichiers inl
-
-*/
