7d41e77649342c7f2d8614d119acd6f62173bb0b
unknown <cjarjoura@gamingcampus.local>
2026-02-05 11:04:17 +0100

    test: add DummyOwner for FSM and state testing

--- FiniteStateMachine/src/test/DummyOwner.hpp
+++ FiniteStateMachine/src/test/DummyOwner.hpp
@@ -2,22 +2,65 @@
 #include "math/vec2.hpp"
 #include <string>
 
-class DummyOwner {
-public: 
-	float moveSpeed = 0;
-	float detectRadius = 0;
-	float lostRadius = 0;
-	float attackRange = 0;
-
-	int pos;
-	int playerPos;
-		float hp;
-		std::string debugStateName;
-		
-	void wanderDir();
-	float wanderTimer();
-	float spawnTimer();
-	bool attackReady();
-	bool didAttackThisFrame();
-
+struct Stats {
+	float vitesse;
+	float detectRadius;
+	float lostRadius;
+	float attackRange;
 };
+
+struct MemoireIa {
+	Vec2 wanderDir;
+	float wanderTimer;
+	float spawnTimer;
+	bool attackReady;
+	bool didAttackThisFrame;
+};
+
+struct DummyOwner {
+	Vec2 pos;
+	Vec2 playerPos;
+	float hp;
+	Stats stats;
+	MemoireIa memoireIa;
+	std::string debugStateName;
+
+	Vec2 getPos() const {
+		return pos;
+	};
+
+	Vec2 getPlayerPos() const {
+		return playerPos;
+	};
+	
+	float getHP() const {
+		return hp;
+	};
+
+	const Stats& getStats() const {
+		return stats;
+	};
+
+	bool attackReady() const {
+		return memoireIa.attackReady;
+	};
+
+	void setDebugStateName(const std::string& name) {
+		debugStateName = name;
+	}
+	
+	void moveToward(const Vec2& target, float dt) {
+		Vec2 dir = (target - pos).normalized();
+		pos = pos + dir * stats.vitesse * dt;
+	}
+
+	void requestAttack() {
+		memoireIa.didAttackThisFrame = true;
+		memoireIa.attackReady = false;
+	}
+
+	void resetFrameFlags() {
+		memoireIa.didAttackThisFrame = false;
+	}
+};
+
